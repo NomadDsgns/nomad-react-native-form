f972874007b59650f21a8de3305ecf636a5b8877
NomadDsgns <90428134+NomadDsgns@users.noreply.github.com>
2021-09-11 02:24:24 -0500

    Update NomadForm.js

--- NomadForm.js
+++ NomadForm.js
@@ -1,43 +1,43 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, TextInput, Text, Button, StyleSheet } from 'react-native';
 
 export default function({ fields, onSubmit }) {
-    const [ state, setState ] = useState({}); //initialize form state for field storage
+    const [ state, setState ] = useState(); //initialize form state for field storage
     const [ isError, setIsError ] = useState(false);
-
-    const keys = useRef(Object.keys(fields)); //create array of keys to iterate through
-
+    
+    const keys = useRef([]);
 
     useEffect(() => { //generate state object on component mounting
         let newState = {};
-        keys.map(key => { //iterate through keys and create initial state values
+        keys.current = Object.keys(fields);
+        keys.current.map(key => { //iterate through keys and create initial state values
             newState = {
                 ...newState, //use spread operator to retain previous elements added to object
                 [key]: { value: '', error: '' }
             }
         });
         setState(newState); //update state with initial values of input
-    }, [])
+    }, [ ])
 
     return (
         <View style={styles.container}>
-            { //iterate and create inputs 
-                keys.map(key => {
-                    return (<View style={styles.inputContainer}>
-                                <Text style={styles.label}>{fields[key].label || ''}</Text>
-                                <TextInput  key={key} 
-                                            style={styles.input}
+            {  keys.current.length > 0 && //iterate and create inputs
+                keys.current.map(key => {
+                    return (<View key={key} 
+                                  style={styles.inputContainer}>
+                                <Text style={styles.label}>{fields[key].label || ''}:</Text>
+                                <TextInput  style={styles.input}
                                             onChangeText={(value) => {
                                                 let error = false;
-                                                setIsError(false)
+                                                setIsError(false);
                                                 fields[key].validators.map(validator => {
-                                                    error = validator(value); //set error value if validator returns false (validator should return string if error is found, false if not)
-                                                    setIsError(true);
-                                                    if (error) break; //breaks out of loop upon first error
+                                                    error = validator(value) || error; //set error value if validator returns false (validator should return string if error is found, false if not)
+                                                    if (error) setIsError(true)
                                                 });
                                                 setState({ ...state, [key]: { value, error }}); //update state with new values
                                             }}
-                                            value={state[key].value}                                            
+                                            value={state[key].value} 
+                                            placeholder={fields[key].required ? "required" : "optional"}                                           
                                             {...fields[key].props} />
                                 <Text style={styles.errorText}>{state[key].error /* display error message if one is found */}</Text>
                             </View>)
@@ -45,8 +45,24 @@ export default function({ fields, onSubmit }) {
             }
             <Button style={styles.button}
                     title="Submit"
-                    onPress={() => onSubmit(state)} /** pass state to submit handler */
-                    disabled={!isError} />
+                    onPress={() => {
+                        for (const [key, value] of Object.entries(state)) { 
+                            if (fields[key].required && value.value === "") { //verify all required fields have a value
+                                value.error = "field required";
+                                setIsError(true);
+                                return;
+                            }
+                            if (value.error === "" || value.error) { //check if error is present or if field has been validated yet
+                                if (fields[key].validators.length > 0) { //if not validation is required, skip validation for field
+                                    fields[key].validators.map(validator => value.error = validator(value.value)); //validate field and set error message
+                                    setIsError(true); //update isError and re-render
+                                    return;
+                                }
+                            }
+                        }
+                        onSubmit(state); /** pass state to submit handler */
+                    }} 
+                    disabled={isError} />
             <Text style={styles.errorText}>{isError ? `Please resolve errors before submitting` : ``}</Text>
         </View>
     )
@@ -69,6 +85,9 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.5,
         shadowRadius: 5,
     },
+    label: {
+        paddingBottom: 5
+    },  
     input: {
         height: 40,
         width: 300,
@@ -81,28 +100,6 @@ const styles = StyleSheet.create({
         textAlign: 'center',
     },
     button: {
-
+        backgroundColor: "#fff"
     }
 })
-
-/*
-    example fields object:
-
-    fields: {
-        email: {
-            validators: [validateLength, validateEmail],
-            label: 'E-mail',
-            props: {
-                placeholder: "required"   
-            }
-        },
-        password: {
-            validators: [validateLength, validateHasNumber],
-            label: 'password',
-            props: {
-                secureEntry: true,
-                placeholder: "required"
-            }
-        }
-    }
-*/
